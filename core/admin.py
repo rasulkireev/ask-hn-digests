2a58855d9d4d092a714da61f0ec5b7e0c272afd1
Rasul Kireev <me@rasulkireev.com>
2025-05-26 09:37:33 -0300

    add ns to admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
-
 from django.contrib import admin
 
-from core.models import BlogPost, HNDiscussionSummary
+from core.models import BlogPost, HNDiscussionSummary, NewsletterSubscriber
 
 admin.site.register(BlogPost)
 admin.site.register(HNDiscussionSummary)
+admin.site.register(NewsletterSubscriber)
