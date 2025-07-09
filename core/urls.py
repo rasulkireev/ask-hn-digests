84554cda0c714b49928925f1c54294e082b910c4
Rasul Kireev <me@rasulkireev.com>
2025-07-09 12:45:37 -0300

    fix

--- core/urls.py
+++ core/urls.py
@@ -11,15 +11,6 @@ urlpatterns = [
     path("settings/", views.UserSettingsView.as_view(), name="settings"),
     # blog
     path("blog/", views.BlogView.as_view(), name="blog_posts"),
-    path("blog/<slug:slug>/", views.BlogPostView.as_view(), name="blog_post"),
-    # app
-    path("api/", api.urls),
-    # utils
-    path("resend-confirmation/", views.resend_confirmation_email, name="resend_confirmation"),
-    path("admin-panel/", AdminPanelView.as_view(), name="admin_panel"),
-    path("settings", views.UserSettingsView.as_view(), name="settings"),
-    # blog
-    path("blog/", views.BlogView.as_view(), name="blog_posts"),
     path("blog/<slug:slug>", views.BlogPostView.as_view(), name="blog_post"),
     # app
     path("api/", api.urls),
